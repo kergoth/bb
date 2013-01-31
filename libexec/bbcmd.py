12da08acbc15f66eba7965f18ae4ec9fc6282699
Christopher Larson <chris_larson@mentor.com>
2013-01-31 09:07:50 -0700

    bbcmd: add initial taskdata bits to tinfoil
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- libexec/bbcmd.py
+++ libexec/bbcmd.py
@@ -23,8 +23,6 @@ class Terminate(BaseException):
     pass
 
 
-# TODO: Let bb.tinfoil.Tinfoil support output files other than stdout, and to
-# enable color support in the formatter when it's a tty.
 class Tinfoil(bb.tinfoil.Tinfoil):
     def __init__(self, output=sys.stdout):
         # Needed to avoid deprecation warnings with python 2.6
@@ -41,7 +39,69 @@ class Tinfoil(bb.tinfoil.Tinfoil):
                                          initialenv)
         self.config_data = self.cooker.configuration.data
         bb.providers.logger.setLevel(logging.ERROR)
+        bb.taskdata.logger.setLevel(logging.CRITICAL)
         self.cooker_data = None
+        self.taskdata = None
+
+    def prepare_taskdata(self, provided=None, rprovided=None):
+        self.cache_data = self.cooker.status
+
+        self.localdata = bb.data.createCopy(self.config_data)
+        self.localdata.finalize()
+        # TODO: why isn't expandKeys a method of DataSmart?
+        bb.data.expandKeys(self.localdata)
+
+        if self.taskdata is None:
+            self.taskdata = bb.taskdata.TaskData(abort=False)
+
+        if provided:
+            self.add_provided(provided)
+
+        if rprovided:
+            self.add_rprovided(rprovided)
+
+    def add_rprovided(self, rprovided):
+        for item in rprovided:
+            self.taskdata.add_rprovider(self.localdata, self.cache_data, item)
+
+        self.taskdata.add_unresolved(self.localdata, self.cache_data)
+
+    def add_provided(self, provided):
+        if 'world' in provided:
+            if not self.cache_data.world_target:
+                self.cooker.buildWorldTargetList()
+            provided.remove('world')
+            provided.extend(self.cache_data.world_target)
+
+        if 'universe' in provided:
+            provided.remove('universe')
+            provided.extend(self.cache_data.universe_target)
+
+        for item in provided:
+            self.taskdata.add_provider(self.localdata, self.cache_data, item)
+
+        self.taskdata.add_unresolved(self.localdata, self.cache_data)
+
+    def rec_get_dependees(self, targetid, depth=0, seen=None):
+        if seen is None:
+            seen = set()
+
+        for dependee_fnid, dependee_id in self.get_dependees(targetid, seen):
+            yield dependee_id, depth
+
+            for _id, _depth in self.rec_get_dependees(dependee_id, depth+1, seen):
+                yield _id, _depth
+
+    def get_dependees(self, targetid, seen):
+        fnid = self.taskdata.build_targets[targetid][0]
+        dep_fnids = self.taskdata.get_dependees(targetid)
+        for dep_fnid in dep_fnids:
+            if dep_fnid in seen:
+                continue
+            seen.add(dep_fnid)
+            for target in self.taskdata.build_targets:
+                if dep_fnid in self.taskdata.build_targets[target]:
+                    yield dep_fnid, target
 
 
 def setup_logger(logger, output=sys.stderr):
